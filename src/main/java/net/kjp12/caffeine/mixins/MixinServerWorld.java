fe29e77ea8efd747323df039c632094a56cf588f
KJP12 <kjp12.k.joy.programer12@gmail.com>
2020-06-15 02:56:12 -0700

    Updated to 1.16-pre5

--- src/main/java/net/kjp12/caffeine/mixins/MixinServerWorld.java
+++ src/main/java/net/kjp12/caffeine/mixins/MixinServerWorld.java
@@ -2,37 +2,26 @@ package net.kjp12.caffeine.mixins;
 
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.profiler.Profiler;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.MutableWorldProperties;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 @Mixin(ServerWorld.class)
-public abstract class MixinServerWorld extends World {
-    @Shadow private boolean allPlayersSleeping;
+public abstract class MixinServerWorld {
+    @Shadow
+    private boolean allPlayersSleeping;
 
-    @Shadow @Final private List<ServerPlayerEntity> players;
-
-    @Shadow public abstract void method_29199(long l);
-
-    @Shadow protected abstract void wakeSleepingPlayers();
-
-    @Shadow protected abstract void resetWeather();
-
-    protected MixinServerWorld(MutableWorldProperties mutableWorldProperties, DimensionType dimensionType, Supplier<Profiler> supplier, boolean bl, boolean bl2, long l) {
-        super(mutableWorldProperties, dimensionType, supplier, bl, bl2, l);
-    }
+    @Shadow
+    @Final
+    private List<ServerPlayerEntity> players;
+    private int sleeping;
 
     /**
      * @reason Completely overriding the method since there is no reason to keep the vanilla code here.
@@ -41,33 +30,20 @@ public abstract class MixinServerWorld extends World {
     @Overwrite
     public void updateSleepingPlayers() {
         allPlayersSleeping = false;
-        if(!players.isEmpty()) {
-            int c1 = 0, c2 = 0;
-            for(var p : players) {
-                if(p.isSpectator() || p.isCreative()) c1++;
-                else if(p.isSleeping()) c2++;
+        if (!players.isEmpty()) {
+            sleeping = 0;
+            int c1 = 0;
+            for (var p : players) {
+                if (p.isSpectator() || p.isCreative()) c1++;
+                else if (p.isSleeping()) sleeping++;
             }
-            allPlayersSleeping = c2 > 0 && c2 >= (players.size() - c1) * 0.33333333F;
+            allPlayersSleeping = sleeping > 0 && sleeping >= (players.size() - c1) * 0.33333333F;
         }
     }
 
-    @Inject(method="tick(Ljava/util/function/BooleanSupplier;)V", at = @At(value = "HEAD"))
-    public void skipTheNight(CallbackInfo ci) {
-        if(allPlayersSleeping) {
-            for(var p : players) {
-                if(p.isSpectator() || p.isCreative()) continue;
-                if(p.isSleeping() && !p.isSleepingLongEnough()) return;
-            }
-            allPlayersSleeping = false;
-            if (getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE)) {
-                long l = this.properties.getTimeOfDay() + 24000L;
-                method_29199(l - l % 24000L);
-            }
-
-            wakeSleepingPlayers();
-            if (getGameRules().getBoolean(GameRules.DO_WEATHER_CYCLE)) {
-                resetWeather();
-            }
-        }
+    @Redirect(method = "tick(Ljava/util/function/BooleanSupplier;)V",
+            at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;noneMatch(Ljava/util/function/Predicate;)Z"))
+    public boolean caffeine$tick$proxyStream$canSkipTheNight(Stream<ServerPlayerEntity> self, Predicate<ServerPlayerEntity> predicate) {
+        return self.filter(ServerPlayerEntity::isSleepingLongEnough).count() >= sleeping;
     }
 }
